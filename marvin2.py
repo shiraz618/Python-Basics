db9c7d952031197d86bcd90ffec50056f4275105
shiraz618 <53206899+shiraz618@users.noreply.github.com>
2020-07-21 19:32:05 +0530

    Add files via upload

---
+++ marvin2.py
@@ -0,0 +1,11 @@
+paranoid_andriod = "Marvin, the paranoid andriod"
+letters = list(paranoid_andriod)
+for char in letters[:6]:
+    print('\t',char)
+print()    
+for char in letters[-7:]:
+    print('\t'*2,char)
+print()
+for char in letters[12:20]:
+    print('\t'*3,char)
+print()    
